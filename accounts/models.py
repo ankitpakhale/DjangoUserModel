b9a190e8e166d7b90a4316dec5341912f5db2e6a
ankitpakhale <akp3067@gmail.com>
2022-03-14 16:14:50 +0530

    working

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.CustomUser
+# Create your models here.
 
 class CustomUser(AbstractUser):
     pass
