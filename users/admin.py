6934987b284da9f872803c92a2e105f58df02cbe
ankitpakhale <akp3067@gmail.com>
2022-03-14 17:55:55 +0530

    done from backend

--- users/admin.py
+++ users/admin.py
@@ -12,20 +12,22 @@ class UserAdminConfig(UserAdmin):
     ordering = ('-start_date',)
     list_display = ('email', 'user_name', 'first_name',
                     'is_active', 'is_staff')
+
     fieldsets = (
         (None, {'fields': ('email', 'user_name', 'first_name',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
         ('Personal', {'fields': ('about',)}),
     )
+
     formfield_overrides = {
         NewUser.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
     }
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
          ),
     )
-
 
 admin.site.register(NewUser, UserAdminConfig)
--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class UserAccountTests(TestCase):
 
     def test_new_superuser(self):
         db = get_user_model()
+
         super_user = db.objects.create_superuser(
             'testuser@super.com', 'username', 'firstname', 'password')
         self.assertEqual(super_user.email, 'testuser@super.com')
