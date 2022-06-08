d0bed89433c9735ea2d3d35f99cb771489e26921
ankitpakhale <akp3067@gmail.com>
2022-06-08 17:24:13 +0530

    Minor changes

--- user_app/models.py
+++ user_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
