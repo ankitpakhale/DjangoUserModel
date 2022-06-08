966d845112d7f7c5adc85e11b97a15b156787a01
ankitpakhale <akp3067@gmail.com>
2022-06-08 17:03:21 +0530

    Working on django user model

--- DjangoUser/urls.py
+++ DjangoUser/urls.py
@@ -1,7 +1,7 @@
-"""usermodel URL Configuration
+"""DjangoUser URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    # path('', include('users.urls')),
-    # path('accounts/', include('accounts.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('', include('user_app.urls')),
 ]
